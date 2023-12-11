e444234aba623b027a9ec8ade7976f113c677f31
SMatusik <sebastianmatusik95@gmail.com>
2023-12-11 22:56:36 +0100

    test

--- Progem/settings.py
+++ Progem/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # user defined
-    "user.apps.UserConfig",
+    "user",
     "project",
     "task",
     "invitation",
